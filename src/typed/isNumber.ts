cc9d636dbbe62fdcf0cb3da9a25aaf97661b890c
marlonpassos <marlon.passos@cadastra.com>
2024-09-16 19:02:10 +0000

    fix(inNumber)!: now return true for NaN

--- src/typed/isNumber.ts
+++ src/typed/isNumber.ts
@@ -6,9 +6,9 @@
  * ```ts
  * isNumber(0) // => true
  * isNumber('0') // => false
- * isNumber(NaN) // => false
+ * isNumber(NaN) // => true
  * ```
  */
 export function isNumber(value: unknown): value is number {
-  return typeof value === 'number' && !Number.isNaN(value)
+  return typeof value === 'number'
 }
