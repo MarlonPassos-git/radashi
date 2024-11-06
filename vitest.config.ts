00231f8d7b0ebf743741dbb93ab20a13400e7930
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-06 10:19:41 -0500

    chore: exclude src/*.ts modules from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,6 +31,7 @@ export default defineConfig(env => ({
     coverage: {
       thresholds: { 100: true },
       include: ['src/**'],
+      exclude: ['src/*.ts'],
     },
     setupFiles: env.mode === 'benchmark' ? ['benchmarks/globals.ts'] : [],
     typecheck: {
